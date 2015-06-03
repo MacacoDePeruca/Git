eab650c571ef6862789ac774123e355571a49359
MacacoDePeruca <vitornascimentositi@gmail.com>
2015-06-03 14:53:59 -0300

    criação de item masterBola

---
+++ ComunicacaoClienteServidorPokemon/src/ItensRules/masterBola.java
@@ -0,0 +1,29 @@
+package ItensRules;
+
+public class masterBola extends Item  implements EfeitoItemCaptura{
+
+	@Override
+	public double capturaChanceBaixa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double capturaChanceMedia() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double capturaChanceAlta() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double capturaTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
