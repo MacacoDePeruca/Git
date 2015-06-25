42650bfc78a424b886084220867f30f63bf1ae32
MacacoDePeruca <vitornascimentositi@gmail.com>
2015-06-24 22:25:08 -0300

    commit thread receber mensagem

--- ComunicacaoClienteServidorPokemon/src/br/com/pokemonUpe/comunication/ThreadReceberMsgConxeao.java
+++ ComunicacaoClienteServidorPokemon/src/br/com/pokemonUpe/comunication/ThreadReceberMsgConxeao.java
@@ -1,9 +1,14 @@
 package br.com.pokemonUpe.comunication;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+
+import br.com.pokemonUpe.gameRules.Pokemon;
 
 public class ThreadReceberMsgConxeao extends Thread{
 
@@ -22,6 +27,8 @@ public class ThreadReceberMsgConxeao extends Thread{
 
 	public void run(){
 		try {
+			DataOutputStream out = (DataOutputStream) conexao.getOutputStream();
+			
 			BufferedReader entrada = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
 			PrintStream saidaCliente = new PrintStream(conexao.getOutputStream());
 			PrintStream saidaServidor = new Servidor().getServBalanServJogo();
@@ -37,30 +44,58 @@ public class ThreadReceberMsgConxeao extends Thread{
 				case "01" :
 						System.out.println("jogo novo! "+s[1]);
 						saidaCliente.println("08&começou o jogo");
-						saidaServidor.println("100&"+ msg.substring(2));
+						//saidaServidor.println("100&"+ msg.substring(2));
 						
 					break;
 				case "02" :
-					
+					//continuar jogo
 					break;
 				case "03" :
-
+					//pokedex
 					break;
 				case "04" :
-
+					//amigos
 					break;
 				case "05" :
-
+					//status
 					break;
 				case "06" :
-
+					//meus pokemons
+					
 					break;
 				case "07" :
-					System.out.println("enviado do servidor " + s[1]);
+					//trocar pokemons
 
 					break;
 				case "08" :
-					System.out.println(s[1]);
+					// batalha
+					
+					break;
+				case "80" :
+					// o calculo do ataque é feito no cliente
+					
+					// opções da batalha ; ataque
+					//80&id_jogador_atacante&id_pokemon&DanoBasico&id_jogador_defensor
+					System.out.println("entrou no 80"+s[1]);
+					//saidaCliente.println("06&tem que escrever em algum lugar!"+s[1]);
+						
+					out.writeUTF(s[1]);
+					
+					break;
+				case "81" :
+					// opções da batalha : defesa
+					break;
+				case "82" :
+					// opções da batalha : golpe especial
+					break;
+				case "83" :
+					// opções da batalha : desistir da batalha
+					break;
+				case "84" :
+					// opções da batalha : usar itens 
+					break;
+				case "85" :
+					// opções da batalha : trocar de pokemon
 					break;
 				case "100":
 					System.out.println("entrou no 100 >>>"+msg);
